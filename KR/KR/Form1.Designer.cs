9662e4556baa352283d7fa112478899290a7798c
Nick <npk001@mail.ru>
2020-03-02 22:23:17 +0300

    First commit

---
+++ KR/KR/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace KR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataMng = new System.Windows.Forms.Button();
+            this.DataRepr = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DataMng
+            // 
+            this.DataMng.Location = new System.Drawing.Point(68, 64);
+            this.DataMng.Name = "DataMng";
+            this.DataMng.Size = new System.Drawing.Size(187, 170);
+            this.DataMng.TabIndex = 0;
+            this.DataMng.Text = "Управление данными";
+            this.DataMng.UseVisualStyleBackColor = true;
+            // 
+            // DataRepr
+            // 
+            this.DataRepr.Location = new System.Drawing.Point(349, 64);
+            this.DataRepr.Name = "DataRepr";
+            this.DataRepr.Size = new System.Drawing.Size(187, 170);
+            this.DataRepr.TabIndex = 0;
+            this.DataRepr.Text = "Учет";
+            this.DataRepr.UseVisualStyleBackColor = true;
+            this.DataRepr.Click += new System.EventHandler(this.DataRepr_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(252, 286);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(106, 39);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "Выход";
+            this.Exit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 372);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.DataRepr);
+            this.Controls.Add(this.DataMng);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DataMng;
+        private System.Windows.Forms.Button DataRepr;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ KR/KR/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace KR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void DataRepr_Click(object sender, EventArgs e)
+        {
+            Form newfrm = new Form2();
+            newfrm.Show();
+            this.Hide();
+        }
+    }
+
+    class Warehouse
+    {
+        public int Id { get; set; }
+        public List<Goods> Goods { get; set; }
+
+        public Warehouse(int id, List<Goods> goods)
+        {
+            Id = id;
+            Goods = goods;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Склад номер: {0}", Id);
+        }
+
+        public void Show_goods()
+        {
+            for (int i = 0; i < Goods.Count; i++)
+            {
+
+            }
+        }
+    }
+
+    class Goods
+    {
+        public int Id { get; set; }
+        public string Categ { get; set; }
+        public string Model { get; set; }
+        public string Producer { get; set; }
+        public int Worth { get; set; }
+        //==============================================================================================
+
+        public Goods(ArrayList list)
+        {
+            Id = Convert.ToInt32(list[0]);
+            Categ = (string)list[1];
+            Model = (string)list[2];
+            Producer = (string)list[3];
+            Worth = Convert.ToInt32(list[4]);
+        }
+        //==============================================================================================
+
+        public override string ToString()
+        {
+            return string.Format("\nИдентификационный номер: {0}, \nкатегория товара: {1}, \nмодель: {2}," +
+                " \nпроизводитель: {3}, \nцена: {4} р.", Id, Categ, Model, Producer, Worth);
+        }
+    }
+
+    class Microphone : Goods
+    {
+        public string SysType { get; set; }
+        public string Sence { get; set; }
+        public int Resist { get; set; }
+        //==============================================================================================
+
+        public Microphone(ArrayList list) : base(list)
+        {
+            SysType = (string)list[5];
+            Sence = (string)list[6];
+            Resist = Convert.ToInt32(list[7]);
+
+        }
+        //==============================================================================================
+
+        public override string ToString()
+        {
+            return string.Format("\nИдентификационный номер: {0}, \nкатегория товара: {1}, \nмодель: {2}," +
+                " \nпроизводитель: {3}, \nцена: {4} р., \nтип системы: {5}," +
+                " \nчувствительность (направленность): {6}, \nсопротивление: {7} Ом",
+                Id, Categ, Model, Producer, Worth, SysType, Sence, Resist);
+        }
+    }
+
+    class Speeckers : Goods
+    {
+        public string SysType { get; set; }
+        public int Power { get; set; }
+        public int DynCount { get; set; }
+        //==============================================================================================
+
+        public Speeckers(ArrayList list) : base(list)
+        {
+            SysType = (string)list[5];
+            Power = (int)list[6];
+            DynCount = (int)list[7];
+        }
+        //==============================================================================================
+
+        public override string ToString()
+        {
+            return string.Format("\nИдентификационный номер: {0}, \nкатегория товара: {1}, \nмодель: {2}," +
+                " \nпроизводитель: {3}, \nцена: {4} р., \nтип системы: {5}, \nмощность: {6} дБ," +
+                " \nколичество динамиков: {7}", Id, Categ, Model, Producer, Worth, SysType, Power, DynCount);
+        }
+    }
+
+    class Players : Goods
+    {
+        public string SysType { get; set; }
+        public string Connect { get; set; }
+        public double HarmDist { get; set; }
+        //==============================================================================================
+
+        public Players(ArrayList list) : base(list)
+        {
+            SysType = (string)list[5]; ;
+            Connect = (string)list[6]; ;
+            HarmDist = (double)list[7]; ;
+        }
+        //==============================================================================================
+
+        public override string ToString()
+        {
+            return string.Format("\nИдентификационный номер: {0}, \nкатегория товара: {1}, \nмодель: {2}," +
+                " \nпроизводитель: {3}, \nцена: {4} р., \nтип системы: {5}, \nспособы соединения: {6}," +
+                " \nкоэффициент гармонического искажения: {7}%", Id, Categ, Model, Producer, Worth, SysType, Connect, HarmDist);
+        }
+    }
+}
